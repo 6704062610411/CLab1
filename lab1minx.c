83559ca9283fb90d0c16bcee6b85cb63d57e59ac
6704062610411 <s6704062610411@email.kmutnb.ac.th>
2024-12-03 15:42:56 +0700

    LAB1.2

--- lab1minx.c
+++ lab1minx.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
+
 int main()
 {
-	int a=20;
-	int b=10;
-	
-    if (b>a)
+	int n1;
+	int n2;
+	scanf("%d",&n1);
+	scanf("%d",&n2);
+
+    if (n2>n1)
     {
-        printf("%d geater than %d",b,a);
+        printf("%d geater than %d",n2,n1);
     }
-    if (a>b)
+    if (n1>n2)
     {
-        printf("%d geater than %d",a,b);
+        printf("%d geater than %d",n1,n2);
     }
     return 0;
 }
