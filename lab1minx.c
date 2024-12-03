9eacfac76128141fad144c586078c349561b7e0f
6704062610411 <s6704062610411@email.kmutnb.ac.th>
2024-12-03 15:43:22 +0700

    LAB1.3

--- lab1minx.c
+++ lab1minx.c
@@ -2,19 +2,18 @@
 
 int main()
 {
-	int n1;
-	int n2;
-	scanf("%d",&n1);
-	scanf("%d",&n2);
+
+	int n1,n2;
+	scanf("%d%d",&n1,&n2);
 
     if (n2>n1)
     {
-        printf("%d geater than %d",n2,n1);
+        printf("%d geater than %d\n",n2,n1);
+        printf("greater than %d",(n2-n1));
     }
-    if (n1>n2)
+    else
     {
-        printf("%d geater than %d",n1,n2);
+        printf("%d geater than %d\n",n1,n2);
+        printf("greater than %d",(n1-n2));
     }
     return 0;
-}
-
