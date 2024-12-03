e76fb97c05a64141b0071054180aedaa6ed7d8e4
6704062610411 <s6704062610411@email.kmutnb.ac.th>
2024-12-03 15:42:29 +0700

    LAB1.1

---
+++ lab1minx.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+	int a=20;
+	int b=10;
+	
+    if (b>a)
+    {
+        printf("%d geater than %d",b,a);
+    }
+    if (a>b)
+    {
+        printf("%d geater than %d",a,b);
+    }
+    return 0;
+}
+
